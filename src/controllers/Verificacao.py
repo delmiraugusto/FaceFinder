2ddf2fc2a8d04733494379ddcf259f7a381eacdf
delmiraugusto <delmir19.da@gmail.com>
2025-09-05 00:02:29 -0300

    Feat: Ia para a Verificacao dos rostos e retorno de porcentagem e true ou false funcionando

---
+++ src/controllers/Verificacao.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, jsonify
+from flask_restful import Resource
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+from deepface import DeepFace
+import cv2
+import numpy as np
+import base64
+
+class Verificacao(Resource):
+
+    def base64_to_cv2_image(base64_str):
+        img_data = base64.b64decode(base64_str)
+        np_arr = np.frombuffer(img_data, np.uint8)
+        img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+        return img
+    
+    def crop_face(img, face_area):
+        x, y, w, h = face_area['x'], face_area['y'], face_area['w'], face_area['h']
+
+        return img[y:y+h, x:x+w]
+
+    def post(self):
+        try:
+            data = request.json
+            
+            imgDocument = Verificacao.base64_to_cv2_image(data['document'])
+            imgSelfie = Verificacao.base64_to_cv2_image(data['selfie'])
+
+            detector = "opencv"
+            align = True
+
+            obj = DeepFace.verify(
+                img1_path=imgDocument,
+                img2_path=imgSelfie,
+                detector_backend=detector,
+                align=align
+            )
+
+            # face_doc = obj['facial_areas']['img1']
+            # face_selfie = obj['facial_areas']['img2']
+
+            # cropped_doc = Verificacao.crop_face(imgDocument, face_doc)
+            # cropped_selfie = Verificacao.crop_face(imgSelfie, face_selfie)
+
+            return jsonify({
+                "verified": obj["verified"],
+                "distance": obj["distance"],
+                "similarity_percent": round((1 - obj["distance"]) * 100, 2),
+            })
+        
+        except Exception as e:
+            return jsonify({"error": str(e)}), 400
--- src/routes/endpoints.py
+++ src/routes/endpoints.py
@@ -1,4 +1,7 @@
 from src.controllers.HelloWorld import Hello
+from src.controllers.Verificacao import Verificacao
 
 def initialize_endpoints(api):
     api.add_resource(Hello, "/hello")
+
+    api.add_resource(Verificacao, "/verify_faces")
