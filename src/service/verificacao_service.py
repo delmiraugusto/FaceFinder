491abb1f6b1660d577eb59e771e6ae615b974878
delmiraugusto <delmir19.da@gmail.com>
2025-09-08 11:59:07 -0300

    Feat: Validar qualidade das imagens

--- src/service/verificacao_service.py
+++ src/service/verificacao_service.py
@@ -9,10 +9,23 @@ def base64_to_cv2_image(base64_str):
     img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
     return img
 
-def verify_faces(document_base64, selfie_base64):
-    imgDocument = base64_to_cv2_image(document_base64)
-    imgSelfie = base64_to_cv2_image(selfie_base64)
+def validate_image_quality(img, min_size=100, blur_threshold=100.0, min_brightness=50, max_brightness=200):
+    
+    if img.shape[0] < min_size or img.shape[1] < min_size:
+        raise ValueError("A imagem é muito pequena para análise.")
 
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    variance = cv2.Laplacian(gray, cv2.CV_64F).var()
+    if variance < blur_threshold:
+        raise ValueError("A imagem está borrada, envie uma foto mais nítida.")
+
+    mean_brightness = np.mean(gray)
+    if mean_brightness < min_brightness or mean_brightness > max_brightness:
+        raise ValueError("A imagem está muito escura ou muito clara.")
+
+    return True
+
+def verify_Presente_face(imgDocument, imgSelfie):
     try:
         obj = DeepFace.verify(
             img1_path=imgDocument,
@@ -20,8 +33,19 @@ def verify_faces(document_base64, selfie_base64):
             detector_backend="opencv",
             align=True
         )
-    except ValueError as ve:
-        raise ValueError("Nenhum rosto detectado em uma ou ambas as imagens.") from ve
+    except ValueError:
+        raise ValueError("Nenhum rosto detectado em uma ou ambas as imagens.") 
+
+    return obj
+
+def verify_faces(document_base64, selfie_base64):
+    imgDocument = base64_to_cv2_image(document_base64)
+    imgSelfie = base64_to_cv2_image(selfie_base64)
+
+    validate_image_quality(imgDocument)
+    validate_image_quality(imgSelfie)
+
+    obj = verify_Presente_face(imgDocument, imgSelfie)
 
     return {
         "verified": obj["verified"],
